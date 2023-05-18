74a13c889de7910a018697e00308c352bfff5dee
tsytsyk.svitlana <tsytsyk.svetlana@gmail.com>
2023-05-18 23:00:11 +0300

    month list added

--- challenges/views.py
+++ challenges/views.py
@@ -18,6 +18,17 @@ monthly_challenges={
 }
 # Create your views here.
 
+def index(request):
+    list_items=""
+    months=list(monthly_challenges.keys())
+    for month in months:
+        capitalized_month=month.capitalize()
+        month_path=reverse("month-challenge", args=[month])
+        list_items+=f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    response_data=f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_number(request, month):
     months=list(monthly_challenges.keys())
     if month>len(month):
@@ -34,4 +45,3 @@ def monthly_challenge(request, month):
         return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("<h1>This month is not supported</h1>")
-    return HttpResponse(challenge_text)
